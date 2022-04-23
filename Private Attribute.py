7450b90797fdba6e5970dd10dc702c5274f0597f
Khifransyah <96522082+khifrvn@users.noreply.github.com>
2022-04-23 21:32:31 +0700

    Update Private Attribute.py

--- Private Attribute.py
+++ Private Attribute.py
@@ -25,6 +25,7 @@ class mahasiswa():
 
 
 # main program
+# main program
 otong = mahasiswa("Otong Surotong", 13332221)  #  ------> otong adalah mahasiswa (OBJEK)
 ucup = mahasiswa("Ucup kucup", 23332221)
 
